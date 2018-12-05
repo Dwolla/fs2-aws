ca10f5712009b7fbbfbe136a5ca26c491cdcf675
Kent Schlorff <kschlorff@dwolla.com>
2018-12-05 15:10:39 -0600

    Adding listBucket method to S3 client

--- main/src/main/scala/com/dwolla/fs2aws/s3/S3Client.scala
+++ main/src/main/scala/com/dwolla/fs2aws/s3/S3Client.scala
@@ -1,16 +1,20 @@
 package com.dwolla.fs2aws.s3
 
 import cats.effect._
+import cats.implicits._
 import com.amazonaws.event.ProgressEvent
 import com.amazonaws.event.ProgressEventType._
 import com.amazonaws.services.s3.model._
 import com.amazonaws.services.s3.transfer._
 import com.amazonaws.services.s3.transfer.internal._
+import com.dwolla.fs2utils.Pagination
 import fs2.{io, _}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 
 trait S3Client[F[_]] {
+  def listBucket(bucket: Bucket): Stream[F, Key]
   def uploadSink(bucket: Bucket, key: Key, objectMetadata: ObjectMetadata): Sink[F, Byte]
   def downloadObject(bucket: Bucket, key: Key, blockingExecutionContext: ExecutionContext): Stream[F, Byte]
   def deleteObject(bucket: Bucket, key: Key): Stream[F, Unit]
@@ -59,6 +63,21 @@ object S3Client {
 
         ()
       })
+
+    override def listBucket(bucket: Bucket): Stream[F, Key] =
+      Pagination.offsetUnfoldChunkEval[F, String, Key] { maybeMarker: Option[String] =>
+        val request = new ListObjectsRequest().withBucketName(bucket)
+        maybeMarker.foreach(request.setMarker)
+
+        for {
+          res <- Sync[F].delay(transferManager.getAmazonS3Client.listObjects(request))
+        } yield {
+          val resultChunk = Chunk.seq(res.getObjectSummaries.asScala).map(_.getKey).map(tagKey)
+          val maybeNextMarker = Option(res.getNextMarker)
+
+          (resultChunk, maybeNextMarker)
+        }
+      }
   }
 
   case object TransferFailedEventException extends RuntimeException("S3 transfer failed")
--- main/src/test/scala/com/dwolla/fs2aws/s3/S3ClientSpec.scala
+++ main/src/test/scala/com/dwolla/fs2aws/s3/S3ClientSpec.scala
@@ -5,9 +5,9 @@ import java.io.ByteArrayInputStream
 import cats.effect._
 import cats.effect.concurrent.Deferred
 import cats.implicits._
-import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.AmazonClientException
 import com.amazonaws.event.{ProgressEvent, ProgressListener, _}
+import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model._
 import com.amazonaws.services.s3.transfer._
 import com.amazonaws.services.s3.transfer.internal.S3ProgressListener
@@ -16,43 +16,40 @@ import com.dwolla.fs2aws.s3.S3Client.S3ClientImpl
 import fs2._
 import org.apache.http.client.methods.HttpRequestBase
 import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
 import org.specs2.concurrent.ExecutionEnv
-import org.specs2.execute._
-import org.specs2.matcher.{MatchResult, Matchers}
+import org.specs2.matcher.{IOMatchers, Matchers}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
-import org.specs2.matcher.IOMatchers
+import org.specs2.specification.Scope
 
 class S3ClientSpec(implicit ee: ExecutionEnv) extends Specification with Matchers with Mockito with IOMatchers {
 
   implicit val cs = IO.contextShift(ee.executionContext)
 
-  val tm = mock[TransferManager]
-  val mockS3 = mock[AmazonS3]
-  when(tm.getAmazonS3Client).thenReturn(mockS3)
-  val client = new S3ClientImpl[IO](tm)
-  val bucket = tagBucket("bucket")
-  val key = tagKey("key")
-  val expectedContentType = "text/x-shellscript"
+  trait setup extends Scope {
 
-  val mockTransferManager = IO(mock[TransferManager])
-  val objectMetadata = IO {
-    val om = new ObjectMetadata()
-    om.setContentType(expectedContentType)
-    om
+    val tm = mock[TransferManager]
+    val mockS3 = mock[AmazonS3]
+    when(tm.getAmazonS3Client).thenReturn(mockS3)
+    val client = new S3ClientImpl[IO](tm)
+    val bucket = tagBucket("bucket")
+    val key = tagKey("key")
+    val expectedContentType = "text/x-shellscript"
+
+    val mockTransferManager = IO(mock[TransferManager])
+    val objectMetadata = IO {
+      val om = new ObjectMetadata()
+      om.setContentType(expectedContentType)
+      om
+    }
   }
 
   "S3Client" should {
-    "upload the bytes of the stream to the given S3 location" >> {
+    "upload the bytes of the stream to the given S3 location" in new setup {
       val expectedBytes = (0 until 100).map(_.toByte)
-      val expectedContentType = "text/x-shellscript"
-      val mockTransferManager = IO(mock[TransferManager])
 
-      val objectMetadata = IO {
-        val om = new ObjectMetadata()
-        om.setContentType(expectedContentType)
-        om
-      }
       for {
         deferredUploadArguments <- Deferred[IO, (PutObjectRequest, S3ProgressListener)]
         tm ← mockTransferManager.flatTap(tm ⇒ IO.asyncF[Unit] { mockSetupComplete =>
@@ -91,7 +88,7 @@ class S3ClientSpec(implicit ee: ExecutionEnv) extends Specification with Matcher
       }
     }
 
-    "download the bytes of the stream from the given S3 location" >> {
+    "download the bytes of the stream from the given S3 location" in new setup {
       val obj = mock[S3Object]
       val expected = (0 until 100).toArray.map(_.toByte)
       when(mockS3.getObject(bucket, key)).thenReturn(obj)
@@ -100,14 +97,73 @@ class S3ClientSpec(implicit ee: ExecutionEnv) extends Specification with Matcher
       client.downloadObject(bucket, key, ee.ec).compile.toList must returnValue(equalTo(expected.toList))
     }
 
-    "delete object from the given S3 location" >> {
+    "delete object from the given S3 location" in new setup {
       client.deleteObject(bucket, key).compile.drain must returnOk
 
       there was one(mockS3).deleteObject(bucket, key)
     }
+
+    "list the keys in the given bucket when the results are not truncated" in new setup {
+      val listing = new ObjectListing()
+      val summary1 = new S3ObjectSummary()
+      summary1.setKey("key1")
+      listing.getObjectSummaries.add(summary1)
+      when(mockS3.listObjects(any[ListObjectsRequest])).thenAnswer(new Answer[ObjectListing] {
+        override def answer(invocation: InvocationOnMock): ObjectListing = {
+          val argument = invocation.getArgument[ListObjectsRequest](0)
+
+          argument.getMarker match {
+            case null =>
+              val x = new ObjectListing
+              x.getObjectSummaries.add(objectSummary(1))
+              x.setTruncated(false)
+              x.setNextMarker(null)
+              x
+            case _ => null
+          }
+        }
+      })
+
+      private val output = client.listBucket(tagBucket("bucket"))
+
+      output.compile.toList must returnValue(equalTo(List("key1")))
+    }
+
+    "list the keys in the given bucket when the results are paginated" in new setup {
+      when(mockS3.listObjects(any[ListObjectsRequest])).thenAnswer(new Answer[ObjectListing] {
+        override def answer(invocation: InvocationOnMock): ObjectListing = {
+          val argument = invocation.getArgument[ListObjectsRequest](0)
+
+          argument.getMarker match {
+            case null =>
+              val x = new ObjectListing
+              x.getObjectSummaries.add(objectSummary(1))
+              x.setNextMarker("marker")
+              x.setTruncated(true)
+              x
+            case "marker" =>
+              val x = new ObjectListing
+              x.getObjectSummaries.add(objectSummary(2))
+              x.setTruncated(false)
+              x.setNextMarker(null)
+              x
+            case _ => null
+          }
+        }
+      })
+
+      private val output = client.listBucket(tagBucket("bucket"))
+
+      output.compile.toList must returnValue(equalTo(List("key1", "key2")))
+    }
   }
 
-  private implicit def ioToResult[T](io: IO[MatchResult[T]]): Result = io.unsafeToFuture().await
+  private def objectSummary(i: Int): S3ObjectSummary = {
+    val x = new S3ObjectSummary
+    x.setKey(s"key$i")
+    x
+  }
+
 }
 
 class NoOpUpload extends Upload {
